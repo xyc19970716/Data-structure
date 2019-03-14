720e00d2bb56e3ade3209c4592b3a9964b25d89e
xyc19970716 <465140038@qq.com>
2019-03-14 19:23:18 +0800

    单向链表的创建、遍历显示、插入、删除、查找、获取长度

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -4,43 +4,125 @@
 #include"pch.h"
 #include<cstdio>
 #include<cstdlib>
-
+ 
 struct Node
 {
 	int Data;
 	Node* next;
 };
 
-void create()
+//创建链表
+Node* createList()
 {
-
+	Node* p = (Node*)malloc(sizeof(Node));	//游标
+	Node* head;	//头指针
+	head = p;
+	while (1)
+	{
+		Node* temp = (Node*)malloc(sizeof(Node));
+		scanf("%d", &temp->Data);
+		if (temp->Data == -1)
+		{
+			p->next = NULL;
+			free(temp);
+			break;
+		}
+		else
+		{
+			p->next = temp;
+			p = p->next;
+		}
+	}
+	
+	return head;
 }
-int main()
+
+//获取链表长度
+int getListLengh(Node* head)
 {
-	Node* a = (Node*)malloc(sizeof(Node));
-	Node* b = (Node*)malloc(sizeof(Node));
-	Node* c = (Node*)malloc(sizeof(Node));
-	Node* d = (Node*)malloc(sizeof(Node));
-	Node* p;
-	a->Data = 1;
-	a->next = b;
-	b->Data = 2;
-	b->next = c;
-	c->Data = 3;
-	c->next = d;
-	d->Data = -1;
-	d->next = NULL;
-	p = a;
+	Node* p = head;
+	int temp = 0;
 	while (p->next != NULL)
 	{
-		printf("%d", p->Data);
+		temp++;
 		p = p->next;
 	}
+	return temp;
+}
 
-	free(a);
-	free(b);
-	free(c);
-	free(d);
+//查找元素
+//找到返回指针，没找到返回空指针
+Node* getPositionForValue(Node* head, int Value)
+{
+	while (head->next != NULL)
+	{
+		if (head->Data == Value)
+		{
+			return head;
+		}
+		else
+		{
+			head = head->next;
+		}
+	}
+	return NULL;
+}
+
+//删除链表的节点
+//成功返回true，失败返回false
+bool deleteValueForList(Node* head, int Value)
+{
+	 
+	Node* p = head;
+	Node* per;
+	while (p->next != NULL)
+	{
+		per = p;
+		p = p->next;
+		if (p->Data == Value)
+		{
+			per->next = p->next;
+			return true;
+		}
+	}
+	return false;
+}
+
+//插入节点（在某个节点后插入）
+//成功返回true，失败返回false
+bool insertValueInList(Node* node, int Value)
+{
+	Node* temp = (Node*)malloc(sizeof(Node));
+	temp->Data = Value;
+	temp->next = node->next;
+	node->next = temp;
+	return true;
+}
+
+//在控制台上遍历显示链表
+void ShowListInConsole(Node* head)
+{
+	while (head->next != NULL)
+	{
+		head = head->next;
+		printf("%d ", head->Data);
+	}
+	printf("\n");
+
+}
+
+int main()
+{
+
+	Node* head = createList();
+	ShowListInConsole(head);
+	printf("%d\n", getListLengh(head));
+	Node* find = getPositionForValue(head, 2);
+	printf("%d\n", find->Data);
+	deleteValueForList(head, 2);
+	ShowListInConsole(head);
+	insertValueInList(getPositionForValue(head, 1), 2);
+	ShowListInConsole(head);
     return 0; 
 }
 
